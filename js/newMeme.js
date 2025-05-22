0f33e91038d573410d4c6a1c9f8d7331f5a84abc
tanulo <tanulo@barossgabor.local>
2025-05-22 10:52:22 +0200

    ezjo

---
+++ js/newMeme.js
@@ -0,0 +1,74 @@
+const fileUpload = document.getElementById('#fileUpload')
+let meme = null; 
+
+fileUpload.addEventListener('change', selectPic);
+btnUpload.addEventListener('click',uploadMeme);
+
+
+async function selectPic(){
+    const file = fileUpload.file[0];
+    console.log(file);
+
+    if (file) {
+        meme = file;
+        const reader = new FileReader();
+        reader.onload =(e) => {
+            const newMeme = document.querySelector('newMeme');
+            newMeme.style.backgroundImage=`url(${e.target.result}")`
+        }
+    }
+}
+
+async function uploadMeme(){
+    try {
+        if(meme){
+            const formData= new FormData;
+            formData.append('meme', meme);
+
+            const response= await fetch('http://127.0.0.1:3000' , {
+                method:'POST',
+                body:formData,
+                credentials:'include'
+            });
+        }else{
+            alert('Válassz ki egy képet');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
